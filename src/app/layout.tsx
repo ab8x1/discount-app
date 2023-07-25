2f0ff55c19eacbd2ed1f58edd8cdc95522f03323
ab8x1 <125adam123@gmail.com>
2023-07-25 18:25:09 +0200

    added social card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,25 @@ import StyledComponentsRegistry from '@/lib/registry'
 import { Inter } from 'next/font/google'
 import WalletProvider from '@/components/WalletProvider'
 import Navbar from '@/components/Navbar'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Discount Finance',
   description: 'Discount App',
-
+  twitter: {
+    card: "summary_large_image",
+    site: "Buy crypto at discount & earn fixed returns Beta waitlist ➡️ discount.finance Powered by @APWineFinance",
+    images: "https://beta.discount.finance/social-card.jpg"
+  },
+  openGraph: {
+    images: {
+      url: "https://beta.discount.finance/social-card.jpg",
+      width: 2400,
+      height: 1350,
+    }
+  }
 }
 
 export default function RootLayout({
