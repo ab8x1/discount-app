1324d0f383df451a1f9f45d2ef781deb391fa738
ab8x1 <125adam123@gmail.com>
2024-07-31 17:14:49 +0200

    buy

--- src/components/DealDetails/helpers/buyDeal.ts
+++ src/components/DealDetails/helpers/buyDeal.ts
@@ -1,7 +1,9 @@
 import { Contract, parseUnits } from "ethers";
 import { UserType } from "@/hooks/useUser";
 import DiscountAbi from "@/artifacts/contracts/DiscountV1.sol/DiscountV1.json";
+import ERC20ABI from "@/artifacts/contracts/@openzeppelin/contracts/token/ERC20/IERC20.sol/IERC20.json";
 
+const erc20Abi = ERC20ABI.abi;
 const discountContractAddress = "0x38A90a2E857d4cC8343CF8A57056B8B36B725863";
 const discountV1ABI = DiscountAbi.abi;
 const underlyingToken = "0x0372cE7418865080D82d0B6677a692a2c045e4D3"; //(spectra USDT):
@@ -12,10 +14,15 @@ export default async function buyDeal(
 ){
     return new Promise<string> (async (res, rej) => {
         const { signer } = user;
+        const parsedAmount = parseUnits(amount.toString(), "ether");
+
+        const underlyingERC20Contract = new Contract(underlyingToken, erc20Abi, signer);
+        const txApprove = await underlyingERC20Contract.approve(discountContractAddress, parsedAmount);
+
         const discountContract = new Contract(discountContractAddress, discountV1ABI, signer);
         const tx = await discountContract.buyDiscountedAsset(
             underlyingToken,
-            parseUnits(amount.toString()),
+            parsedAmount,
             "0xD7fd5213B94Cb3B2BBFdeDf5fB5Ac7b5644552b1", //IBT
             "0x080732d65987C5D5F9Aaa72999d7B0e02713aE72", //curvePool
             0,
