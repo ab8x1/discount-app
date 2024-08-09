7bce0de196c7a1635166abb501017cd6305dc982
ab8x1 <125adam123@gmail.com>
2024-08-09 17:45:10 +0200

    disable date neable progress

--- src/components/ThinDeal/ThinDeal.tsx
+++ src/components/ThinDeal/ThinDeal.tsx
@@ -56,17 +56,17 @@ export default function ThinDeal({dealInfo} : {
                     </div>
                 </div>
             </div>
-            <p className={`${styles.claimDate} alignY`}>
+            {/* <p className={`${styles.claimDate} alignY`}>
                 <span>Claim Date</span>
                 <span>{timestampToDate(date.end)}</span>
-            </p>
-            {/* <span className={styles.progressBar} style={{background: progressColor.background}}>
+            </p> */}
+            <span className={styles.progressBar} style={{background: progressColor.background}}>
                 <span
                     className={styles.line}
                     style={{width: `${(Date.now() - date.start)/(date.end - date.start)*100}%`,
                     background: progressColor.line}}
                 />
-            </span> */}
+            </span>
             <div className={`alignY boxButton ${!isEnabled && 'disabledButton'}`}>
                 {
                     isEnabled
