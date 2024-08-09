1216594680703e5c9f35c8aba3426bc235485f40
ab8x1 <125adam123@gmail.com>
2024-08-09 17:08:38 +0200

    test disabled some components

--- src/components/LoadingValue/LoadingValue.tsx
+++ src/components/LoadingValue/LoadingValue.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react"
-// import ReactLoading from "react-loading"
+import ReactLoading from "react-loading"
 
 export default function Name({isLoading, value, loaderHeight ,loaderColor, loaderWidth} : {
     isLoading: boolean,
@@ -11,13 +11,13 @@ export default function Name({isLoading, value, loaderHeight ,loaderColor, loade
 
     return(
         <>
-            {/* <> </>
+            <> </>
             {
                 isLoading ?
                 <ReactLoading type='bubbles' width={loaderWidth} height={loaderHeight} color={loaderColor}/>
                 : value
             }
-            <> </> */}
+            <> </>
         </>
     )
 }
