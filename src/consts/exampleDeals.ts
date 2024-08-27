19b7f313fcdffd651cf0ded774ca2169d74a3258
ab8x1 <125adam123@gmail.com>
2024-08-27 16:21:46 +0200

    changed dai icon

--- src/consts/exampleDeals.ts
+++ src/consts/exampleDeals.ts
@@ -2,8 +2,8 @@ import { OfferType } from "@/types/offer"
 
 export const exampleOffers: OfferType[] = [
     {
-        id: '1', //arb
-        token: 'fDAi', //arb
+        id: '1',
+        token: 'DAi',
         tokenImgUrl: '/tokens/DAI.svg',
         background: 'linear-gradient(180deg, #FFC566 0%, #E08E0B 100%)',
         progressColor: {
@@ -13,14 +13,14 @@ export const exampleOffers: OfferType[] = [
         isEnabled: true,
         date: {
             start: 0,
-            end: 1727906400000 //arb
+            end: 1727906400000
         },
-        ptAddress: "0x458e662aa15a3E338A9b1f9668e2CCb302Fd4340", //arb
-        underlyingTokenAddress: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", //arb
+        ptAddress: "0x458e662aa15a3E338A9b1f9668e2CCb302Fd4340",
+        underlyingTokenAddress: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
         chainName: "Arbitrum",
-        chainHexId: "0xa4b1", //arb
+        chainHexId: "0xa4b1",
         chainRpcUrl: process.env.NEXT_PUBLIC_ARBITRUM_RPC as string,
-        curvePool: "0xf5650B7dd5216fCc6cfa5C009FFd147eD8254f37", //arb
+        curvePool: "0xf5650B7dd5216fCc6cfa5C009FFd147eD8254f37",
         IBTindexInCurvePool: 0,
         PTindexInCurvePool: 1
     }
