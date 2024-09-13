1077816de4e02970086a3b14086ea4c753a43370
ab8x1 <125adam123@gmail.com>
2024-09-13 13:58:35 +0200

    fixed unsuported network error

--- src/consts/globalConsts.ts
+++ src/consts/globalConsts.ts
@@ -2,11 +2,14 @@ type ContractAddress = {
     [key: string]: string
   }
 
+  const contracts: ContractAddress = {
+    "0x1": "0x209AB2a2340EA3DaF049d3FF4617f7992B01a8D6",
+    "0xa4b1": "0x2470275bF66Ac891a27179F19422ed71a3191514",
+  }
+
 export function getDisocuntContractAddress(id?: string){
-      const contracts: ContractAddress = {
-        "0x1": "0x209AB2a2340EA3DaF049d3FF4617f7992B01a8D6",
-        "0xa4b1": "0x2470275bF66Ac891a27179F19422ed71a3191514",
-        // "0xaa36a7": "0xe8eF3902B94d08f4d740FBBF368B4D1a4C5fD731" //sepolia testnet
-      }
-      return contracts[id || "0x1"];
+    let address = contracts["0x1"];
+    if(id && contracts[id])
+      address = contracts[id];
+    return address;
 }
